418baee6e4f3b74e406da85ffd7e8d2a39641e23
leiZhongHan <56380115+leiZhongHan@users.noreply.github.com>
2020-04-23 19:52:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// an oreo cookie weighs 14.5 grams
+let oreo = 14.5;
+
+// one pound is equal to 454 grams
+let gramsPerPound = 500;
+
+let studentWeight;
+
+$("button").click(function() {
+    studentWeight = $("input").val();
+    let oreos = ( studentWeight * gramsPerPound ) / oreo;    
+    $("img").attr("src", "giphy.gif");
+    $("#oreoWeight").text("你的奥利奥体重是 " + oreos + " 奥利奥!");
+
+    if (oreos > 100) {
+        window.alert("呐，看来阁下应该去打篮球了呢")
+    }
+});
+
+
+
