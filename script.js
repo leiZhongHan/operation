8a394774347f108b749f33cf2851c7527f78b73e
leiZhongHan <56380115+leiZhongHan@users.noreply.github.com>
2020-04-23 20:08:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ let oreo = 14.5;
 // one pound is equal to 454 grams
 let gramsPerPound = 454;
 
-gramsPerPound = 500;
-
 let studentWeight;
 
 $("button").click(function() {
     studentWeight = $("input").val();
-    let oreos = ( studentWeight * gramsPerPound ) / oreo;    
+    let oreos = ( studentWeight * gramsPerPound * 0.9071847 * 500 ) / oreo;    
     $("img").attr("src", "giphy.gif");
     $("#oreoWeight").text("你的奥利奥体重是 " + oreos + " 奥利奥!");
 
