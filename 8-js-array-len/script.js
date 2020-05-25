75cb0c8b3221c66529a409d73a30b83c7af95eed
leiZhongHan <56380115+leiZhongHan@users.noreply.github.com>
2020-05-26 00:32:16 +0800

    Delete script.js

--- 8-js-array-len/script.js
+++
@@ -1,8 +0,0 @@
-let guests = [];
-
-$(".addButton").click(function() {
-    let guest = $(".guestName").val();
-    guests.push(guest);
-    $(".guestList").append('<li>'+guests[guests.length-1]+'</li>'+'<br>');
-    $(".numberOfGuests").text(guests.length);
-});
